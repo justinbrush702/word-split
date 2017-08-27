918a44da622ec955f88ac2398be711ded59792ec
Justin Brush <jbrush@pugetsound.edu>
2017-08-26 23:29:05 -0700

    Refactors recursive solution; adds more info to README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# word-split
-Solutions to the "Word Split" problem
+# Word Split
+This repository houses different solutions to the "Word Split" problem.
+
+### The Problem
+Given an input string and a dictionary, return a splitting of the string into words in the dictionary.
+* If a splitting is possible, return a possible list of words (There can be multiple answers.).
+* If a splitting is impossible, return null.
+
+### Rules
+* Exact Matching - potential words must match exactly to a word in the dictionary, including capital letters
+
+### Examples
+* split("helloworld", ["world", "hello"]) -> ["hello", "world"]
+* split("hello world", ["world", "hello"]) -> null / empty set
+* split("hello world", ["world", "hello", " ", "test"]) -> ["world", "hello", " "]
--- recursive.js
+++ recursive.js
@@ -1,22 +1,20 @@
 /**
-* Word Split
+* Recursive Solution
 *
-* Given an input string and a dictionary, return a splitting of the string into words in the dictionary
+* Look at given string, find breakpoints (good words)
+* Once I find a breakpoint, recurse. Look at the rest of the substring.
 *
-* split("helloworld", ["world", "hello"]) -> ["hello", "world"]
-* split("hello world", ["world", "hello"]) -> null / empty set
-* split("hello world", ["world", "hello", " ", "test"]) -> ["world", "hello", " "]
+* BASE CASE --> empty string (return an empty list)
+* If there is no breakpoint on a word --> return null
 *
-* exact matching
-*
+* NOTE: I reverse the list after receiving it instead of during the back-stepping to decrease runtime.
+* The runtime of reversing the list is O(n). The runtime of "unshifting" the list while back-stepping is O(n^2).
 **/
 
-// Look at given string, find breakpoints (good words)
-// Once I find a breakpoint, recurse. Look at the rest of the substring.
-// BASE CASE --> empty string (return true)
-// No breakpoint on a word --> return false
 
-
+// Wrapper function to recursive function
+// input --> string to be split
+// dictionary --> list of words in the dictionary
 var wordSplit = function (input, dictionary) {
 
   if (input === null || !dictionary) {
@@ -24,12 +22,22 @@ var wordSplit = function (input, dictionary) {
   }
 
   var words = findWords(input, dictionary);
-  return words;
+
+  if (!words) {
+    return null;
+  }
+
+  var reverse = [];
+  while (words.length > 0) {
+    reverse.push(words.pop());
+  }
+
+  return reverse;
 }
 
+// Recursive function
+// Splits subInput into words until it finds a splitting that works (if a splitting exists)
 var findWords = function (subInput, dictionary) {
-
-
 
   // BASE CASE
   if (subInput == "") {
@@ -37,28 +45,27 @@ var findWords = function (subInput, dictionary) {
   } else {
     // Keep track of a cursor
     var index = 0;
-    while (index < subInput.length) {
-      index++; // advance cursor
-      var currWord = subInput.substring(0, index);
-      if (isWord(currWord, dictionary)) {
-        // console.log(currWord);
-        var success = findWords(subInput.substring(index, subInput.length), dictionary);
-        // console.log(success);
 
+    // While there are more characters to check for splitting
+    while (index < subInput.length) {
+      index++; // Advance cursor
+      var currWord = subInput.substring(0, index);
+
+      if (isWord(currWord, dictionary)) {
+        // Recurse down the rest of the input (not including the currWord)
+        var success = findWords(subInput.substring(index, subInput.length), dictionary);
         if (success != null) {
-          success.push(currWord);
+          success.push(currWord); // Build list of words as algorithm back-steps
           return success;
         }
-
       }
     }
+    // Hit the end character, but this string is not a word
     return null;
   }
-
-
 };
 
-
+// Function to determine whether the given characters are a word in the dictionary
 var isWord = function (word, dictionary) {
   for (var i = 0; i < dictionary.length; i++) {
     if (dictionary[i] == word) {
@@ -68,11 +75,7 @@ var isWord = function (word, dictionary) {
   return false;
 }
 
-
-// var dictionary = ["world", "hello", "hell"];
-// var input = "helloworld";
-
-var dictionary = ["I", "hello", "hell", "world", "he"];
 var input = "helloworld";
+var dictionary = ["I", "hello", "hell", "world", "he"];
 
 console.log(wordSplit(input, dictionary));
